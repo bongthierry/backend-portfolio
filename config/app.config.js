181319c098d40660496a3460cd99a338f0de6110
bongthierry <thierrybongjoh@gmail.com>
2023-06-20 16:22:04 -0700

    updated commit

---
+++ config/app.config.js
@@ -0,0 +1,10 @@
+require("dotenv").config;
+
+const MONGO_DB_CONFIG = {
+   DB: 'mongodb+srv://thierrybongjoh:yJWMCYAdMn3yZGFF@reactbackendportfolio.wdgb37o.mongodb.net/?retryWrites=true&w=majority'
+    
+}
+
+module.exports = {
+    MONGO_DB_CONFIG
+}
